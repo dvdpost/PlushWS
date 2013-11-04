702e925ef52a2aeefc24210aa846a4df98b660c2
it dvdpost <it@dvdposr.be>
2013-11-04 21:53:06 +0100

    first commit

---
+++ MobileDevicesService/ResultSetEntities.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MobileDevicesService
+{
+    public class VodStreamingInfo
+    {
+        public int imdb_id;
+        public string filename;
+        public int credits;
+    }
+}
